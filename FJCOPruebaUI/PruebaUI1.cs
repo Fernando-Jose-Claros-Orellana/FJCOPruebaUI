24599c06d38d951cd19be0ac196c66cc39815433
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-16 22:37:15 -0600

    Se modifica clase que prueba crear un producto

--- FJCOPruebaUI/PruebaUI1.cs
+++ FJCOPruebaUI/PruebaUI1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,66 @@ using System.Threading.Tasks;
 
 namespace FJCOPruebaUI
 {
-	public class PruebaUI1
-	{
-	}
+	[TestClass]
+    public class PruebaUI1 : IDisposable
+    {
+        private IWebDriver driver;
+
+        public PruebaUI1()
+        {
+            // Inicializa el controlador de Chrome
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void Blazor()
+        {
+            // Navega a Google
+            driver.Navigate().GoToUrl("https://localhost:7293/product-list");
+
+            // Espera un momento para que se carguen los resultados
+            System.Threading.Thread.Sleep(1000);
+
+            // Encuentra el cuadro de búsqueda
+            var btn = driver.FindElement(By.Name("crear"));
+
+            btn.Click();
+
+            // Espera un momento para que se carguen los resultados
+            System.Threading.Thread.Sleep(1000);
+
+            var nombre = driver.FindElement(By.Id("Nombre"));
+
+            nombre.SendKeys("PruebaN");
+
+            var descripcion = driver.FindElement(By.Id("Descripcion"));
+
+            descripcion.SendKeys("PruebaD");
+
+            var precio = driver.FindElement(By.Id("Precio"));
+
+            precio.SendKeys("100");
+
+            var guardar = driver.FindElement(By.Name("guardar"));
+
+            guardar.Click();
+
+            // Espera un momento para que se carguen los resultados
+            System.Threading.Thread.Sleep(1000);
+
+            var regresar = driver.FindElement(By.Name("regresar"));
+
+            regresar.Click();
+
+            // Espera un momento para que se carguen los resultados
+            System.Threading.Thread.Sleep(1000);
+
+        }
+
+        public void Dispose()
+        {
+            // Cierra el navegador
+            driver.Quit();
+        }
+    }
 }
